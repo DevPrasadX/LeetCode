bedc936533f68db3dda00f5c0197a3d44c8d2eed
Prasad Pansare <152534935+DevPrasadX@users.noreply.github.com>
2025-03-02 02:29:59 +0530

    Time: 2557 ms (5.01%) | Memory: 45.4 MB (36.30%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -0,0 +1,35 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() < 1) {
+            return "";
+        }
+        
+        int maxLength = 0;
+        String longest = "";
+
+        // Try every possible substring
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i; j < s.length(); j++) {
+                String sub = s.substring(i, j + 1);
+                if (isPalindrome(sub) && sub.length() > maxLength) {
+                    longest = sub;
+                    maxLength = sub.length();
+                }
+            }
+        }
+        return longest;
+    }
+
+    // Function to check if a string is a palindrome
+    private boolean isPalindrome(String str) {
+        int left = 0, right = str.length() - 1;
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
