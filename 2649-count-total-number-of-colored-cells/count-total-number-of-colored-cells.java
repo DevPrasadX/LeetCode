8e3051ce994cfeaabc56b1f18d33919deed4840b
Prasad Pansare <152534935+DevPrasadX@users.noreply.github.com>
2025-03-05 23:38:36 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (46.72%) - LeetSync

--- 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.java
+++ 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.java
@@ -1,11 +1,9 @@
 class Solution {
     public long coloredCells(int n) {
-        long num=n;
-        long x=n-1;
-         return num*num + x*x;
-        //       long num = n;  // Convert n to long
-        // return 2 * num * (num - 1) + 1;
-
+        long num=n; //we need to take LONG instead if int because at testcase 52 , result of the calculation exceeded the maximum value of an int
+        long x=n-1; // this will store previous number of input
+        return num*num + x*x; //this is ,y derived mathematical formula
+ 
         
     }
 }
