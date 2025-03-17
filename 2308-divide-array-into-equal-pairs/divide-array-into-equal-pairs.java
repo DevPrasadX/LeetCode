191df94186dd2eb0b60f9f239d9238356663aaaf
Prasad Pansare <152534935+DevPrasadX@users.noreply.github.com>
2025-03-17 23:04:33 +0530

    Time: 4 ms (74.89%) | Memory: 44.3 MB (71.91%) - LeetSync

--- 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.java
+++ 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.java
@@ -1,20 +1,15 @@
 class Solution {
     public boolean divideArray(int[] arr) {
-        Arrays.sort(arr);
-        List <List<Integer>> numbers=new ArrayList<>();
+        Arrays.sort(arr); // we will be sorting the arrays first. so if in pair, we can notice that equal values will obv be next to each other
         boolean hasNonEqualPair=false;
-        if(arr.length%2==0)
+        if(arr.length%2==0) // logically if array size is odd, it cant have pairs with same value inside. at the end, only 1 element will be remained whihc is not paired. so if this condition is not satisfied, obv we will return false
         {
             for (int i = 0; i < arr.length; i += 2) {
-                List<Integer> subarray = new ArrayList<>();
-                subarray.add(arr[i]);
                 if (i + 1 < arr.length) { 
-                    subarray.add(arr[i + 1]);
-                    if (arr[i]!=arr[i + 1]) { 
-                        hasNonEqualPair = true;
+                    if (arr[i]!=arr[i + 1]) {       
+                        hasNonEqualPair = true;  // here we simply check if the returning subarray's values are equal or not. if not equal, we will flag this var as true
                     }
                 }
-                numbers.add(subarray);
             }
         }
         else
