793ba8818f3d596c4e45d8df5cf8dd6e22b7d070
Prasad Pansare <152534935+DevPrasadX@users.noreply.github.com>
2025-03-28 00:19:16 +0530

    Time: 126 ms (66.67%) | Memory: 24.9 MB (66.67%) - LeetSync

---
+++ 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def minimumIndex(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        total_count = Counter(nums)
+        dominant = max(total_count, key=lambda x: total_count[x])
+    
+        # Step 2: Check for valid split
+        left_count, right_count = 0, total_count[dominant]
+        n = len(nums)
+    
+        for i in range(n - 1):
+            if nums[i] == dominant:
+                left_count += 1
+                right_count -= 1
+            
+            if left_count * 2 > (i + 1) and right_count * 2 > (n - i - 1):
+                return i
+    
+        return -1
