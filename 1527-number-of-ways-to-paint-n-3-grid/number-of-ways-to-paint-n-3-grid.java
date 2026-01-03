51f6b8beb9ccacd7d2c18aa9cd6e07197612f4dd
Prasad Pansare <152534935+DevPrasadX@users.noreply.github.com>
2026-01-03 18:47:19 +0000

    Time: 3 ms (93.85%) | Memory: 42.3 MB (41.54%) - LeetSync

---
+++ 1527-number-of-ways-to-paint-n-3-grid/number-of-ways-to-paint-n-3-grid.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numOfWays(int n) {
+        final int MOD = 1_000_000_007;
+
+
+        long dpA = 6; 
+        long dpB = 6; 
+
+        for (int i = 2; i <= n; i++) {
+            long newA = (dpA * 2 + dpB * 2) % MOD;
+            long newB = (dpA * 2 + dpB * 3) % MOD;
+            dpA = newA;
+            dpB = newB;
+        }
+
+        return (int) ((dpA + dpB) % MOD);
+    }
+}
