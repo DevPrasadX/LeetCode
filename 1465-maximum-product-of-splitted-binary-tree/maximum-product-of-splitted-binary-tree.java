7ce7fa44b5a0ef5918d56adfb4e30472ec2d4648
Prasad Pansare <152534935+DevPrasadX@users.noreply.github.com>
2026-01-08 13:46:13 +0000

    Time: 5 ms (99.22%) | Memory: 59.2 MB (87.60%) - LeetSync

---
+++ 1465-maximum-product-of-splitted-binary-tree/maximum-product-of-splitted-binary-tree.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    private long totalSum = 0;
+    private long maxProduct = 0;
+    private static final int MOD = 1_000_000_007;
+
+    public int maxProduct(TreeNode root) {
+        totalSum = getTotalSum(root);
+
+        dfs(root);
+
+        return (int)(maxProduct % MOD);
+    }
+
+    private long getTotalSum(TreeNode node) {
+        if (node == null) return 0;
+        return node.val + getTotalSum(node.left) + getTotalSum(node.right);
+    }
+
+    private long dfs(TreeNode node) {
+        if (node == null) return 0;
+
+        long leftSum = dfs(node.left);
+        long rightSum = dfs(node.right);
+
+        long subTreeSum = node.val + leftSum + rightSum;
+
+        long product = subTreeSum * (totalSum - subTreeSum);
+        maxProduct = Math.max(maxProduct, product);
+
+        return subTreeSum;
+    }
+}
