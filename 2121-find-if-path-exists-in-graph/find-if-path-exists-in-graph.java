dae1a2095b0584c523e12be5f4daecfb7e6e8cd2
Prasad Pansare <152534935+DevPrasadX@users.noreply.github.com>
2026-04-12 23:52:23 +0100

    Time: 125 ms (57.59%) | Memory: 279.4 MB (70.90%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -1,13 +1,12 @@
 class Solution {
-    private boolean dfs(int i, int destination, 
-                        Map<Integer, List<Integer>> map, Set<Integer> set) {
+    private boolean dfs(int i, int destination,
+                        List<List<Integer>> list, boolean[] visited) {
         if (i == destination) return true;
 
-        for (int neighbour : map.getOrDefault(i, new ArrayList<>())) {
-            if (!set.contains(neighbour)) {
-                set.add(neighbour);
-                
-                if (dfs(neighbour, destination, map, set)) {
+        for (int neighbour : list.get(i)) {
+            if (!visited[neighbour]) {
+                visited[neighbour] = true;
+                if (dfs(neighbour, destination, list, visited)) {
                     return true;
                 }
             }
@@ -18,15 +17,20 @@ class Solution {
     public boolean validPath(int n, int[][] edges, int source, int destination) {
         if (source == destination) return true;
 
-        Map<Integer, List<Integer>> map = new HashMap<>();
+        // List instead of HashMap
+        List<List<Integer>> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            list.add(new ArrayList<>());   // create empty list for each node
+        }
         for (int[] edge : edges) {
-            map.computeIfAbsent(edge[0], k -> new ArrayList<>()).add(edge[1]);
-            map.computeIfAbsent(edge[1], k -> new ArrayList<>()).add(edge[0]);
+            list.get(edge[0]).add(edge[1]);
+            list.get(edge[1]).add(edge[0]);
         }
 
-        Set<Integer> set = new HashSet<>();
-        set.add(source);
+        // boolean array instead of HashSet
+        boolean[] visited = new boolean[n];
+        visited[source] = true;
 
-        return dfs(source, destination, map, set);
+        return dfs(source, destination, list, visited);
     }
 }
