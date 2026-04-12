082cf8e316677ea2173f382315e35da70aaba7d4
Prasad Pansare <152534935+DevPrasadX@users.noreply.github.com>
2026-04-12 13:33:58 +0100

    Time: 34 ms (7.51%) | Memory: 46.8 MB (12.24%) - LeetSync

---
+++ 133-clone-graph/clone-graph.java
@@ -0,0 +1,46 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    public HashMap<Node,Node> map = new HashMap<>();
+     public Node dfs(Node node)
+
+        {
+            if(node ==null)
+            {
+                return node;
+            }
+            if (map.containsKey(node))
+            {
+                return map.get(node);
+            }
+            Node copy = new Node(node.val , new ArrayList());
+            map.put(node,copy);
+            System.out.println(map);
+            for (Node neighbor : node.neighbors) {
+            copy.neighbors.add(dfs(neighbor));
+        }
+            return copy;
+        }
+    public Node cloneGraph(Node node) {
+       
+        return dfs(node);
+    }
+}
