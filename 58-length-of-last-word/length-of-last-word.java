b50b2c42c7364dbdb8ffb4b4c4ba2b9caed57126
Prasad Pansare <152534935+DevPrasadX@users.noreply.github.com>
2026-04-21 22:55:09 +0100

    Time: 0 ms (100.00%) | Memory: 43 MB (53.35%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -1,7 +1,19 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-         s = s.trim(); // trim the trailing spaces in the string
-        return s.length() - s.lastIndexOf(" ") - 1;
-        
+        int p2 = s.length() - 1;
+        int counter = 0;
+
+        // Step 1: skip trailing spaces
+        while (p2 >= 0 && s.charAt(p2) == ' ') {
+            p2--;
+        }
+
+        // Step 2: count last word
+        while (p2 >= 0 && s.charAt(p2) != ' ') {
+            counter++;
+            p2--;
+        }
+
+        return counter;
     }
 }
