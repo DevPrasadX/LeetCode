140103f6d5924bacb731b63641015b15c40d0ee9
Prasad Pansare <152534935+DevPrasadX@users.noreply.github.com>
2026-04-21 10:43:51 +0100

    Time: 0 ms (100.00%) | Memory: 46.4 MB (83.36%) - LeetSync

--- 1580-shuffle-the-array/shuffle-the-array.java
+++ 1580-shuffle-the-array/shuffle-the-array.java
@@ -1,27 +1,34 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {
-        List<Integer> list = new ArrayList<>();
-         List<Integer> list2 = new ArrayList<>();
-           List<Integer> list3 = new ArrayList<>();
+        int[] array = new int[nums.length];
+        for(int i=0;i<n;i++)
+        {
+            array[2*i]=nums[i];
+            array[2*i+1] = nums[i+n];
+        }
 
-        for(int i=0;i<n;i++)
-        {
-           list.add(nums[i]);
-        }
-         for(int i=n;i<nums.length;i++)
-        {
-           list2.add(nums[i]);
-        }
-        for(int i=0;i<n;i++)
-        {
-            list3.add(list.get(i));
-            list3.add(list2.get(i));
-        }
-                   int[] array = new int[list3.size()];
-        for(int i=0;i<list3.size();i++)
-        {
-            array[i]=list3.get(i);
-        }
+        // List<Integer> list = new ArrayList<>();
+        //  List<Integer> list2 = new ArrayList<>();
+        //    List<Integer> list3 = new ArrayList<>();
+
+        // for(int i=0;i<n;i++)
+        // {
+        //    list.add(nums[i]);
+        // }
+        //  for(int i=n;i<nums.length;i++)
+        // {
+        //    list2.add(nums[i]);
+        // }
+        // for(int i=0;i<n;i++)
+        // {
+        //     list3.add(list.get(i));
+        //     list3.add(list2.get(i));
+        // }
+        //            int[] array = new int[list3.size()];
+        // for(int i=0;i<list3.size();i++)
+        // {
+        //     array[i]=list3.get(i);
+        // }
 
         return array;
     }
